309243b3a583dae96cba8f4ab8d2a92552da4a66
athiracv12 <athira.chenanveettil@gmail.com>
2026-02-20 12:59:29 +0530

    changes pushed

--- src/GitHub/test.java
+++ src/GitHub/test.java
@@ -3,8 +3,10 @@ package GitHub;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a= 5;
+		int b=10;
+		int c= a+b;
+		System.out.println(c);
 	}
 
 }
